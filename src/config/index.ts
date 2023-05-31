ec10babbd6bbc944fcb52fa9e996cb93fad2d97f
Ilker Satir <isatir1990@gmail.com>
2023-06-01 00:45:22 +0200

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,4 +3,4 @@ export const DEFAULT_TIME_TO_DRAW = 5; // DEFAULT: 5 seconds
 
 // this defines the maximum number of iterations for the drawn tickets
 // + 1 for the null ticket
-export const MAX_ITERATIONS = 24;
+export const MAX_ITERATIONS = 25;
